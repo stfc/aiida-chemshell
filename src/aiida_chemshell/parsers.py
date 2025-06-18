628b871205f7644b84fb1a2359879604cb0bfce1
Benjamin Speake <benjaminspeake@gmail.com>
2025-06-18 10:40:00 +0100

    Add parser for the total system SP energy value

--- src/aiida_chemshell/parsers.py
+++ src/aiida_chemshell/parsers.py
@@ -1,4 +1,5 @@
 from aiida.parsers.parser import Parser 
+from aiida.orm import Float
 
 class ChemShellParser(Parser):
     """
@@ -9,3 +10,13 @@ class ChemShellParser(Parser):
         """
         Parse the output of a ChemShell calculation.
         """
+
+        # Read the single point energy from the ChemShell output 
+        with self.retrieved.open("output.log", 'r') as f:
+            for line in f:
+                if "Final SP energy" in line:
+                    energy = Float(line.split()[4])
+                    self.out("energy", energy)
+                    break 
+
+        

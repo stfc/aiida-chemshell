1878a6f03f00d35595675a9b1c6350eab1ed8a3b
Benjamin Speake <benjaminspeake@gmail.com>
2025-06-19 10:44:41 +0100

    Add ability to run QM based structure optimisation tasks

--- src/aiida_chemshell/calculations.py
+++ src/aiida_chemshell/calculations.py
@@ -12,6 +12,7 @@ class ChemShellCalculation(CalcJob):
 
     FILE_SCRIPT = "chemshell_input.py"
     FILE_STDOUT = "output.log"
+    FILE_DLFIND = "_dl_find.pun"
 
     @classmethod
     def define(cls, spec: CalcJobProcessSpec) -> None:
@@ -20,16 +21,25 @@ class ChemShellCalculation(CalcJob):
         """
         super(ChemShellCalculation, cls).define(spec)
         spec.input('structure', valid_type=SinglefileData, required=True, help="The input structure for the ChemShell calculation contained within an '.xyz', '.pun' or '.cjson' file.")
+        
         spec.input("calculation_parameters", valid_type=Dict, validator=cls.validate_calculation_parameters, required=False, help="A dictionary of parameters for the ChemShell Task object.")
-        spec.input("QM_parameters", valid_type=Dict, validator=cls.validate_QM_parameters, required=False, help="A dictionary of parameters for to be passed to the Theory object for the ChemShell calculation.")
-        spec.input("MM_parameters", valid_type=Dict, validator=cls.validate_MM_parameters, required=False, help="A dictionary of parameters for the ChemShell MM interface.")
-        spec.input("QMMM_parameters", valid_type=Dict, required=False, help="A dictionary of parameters for the ChemShell QM/MM interface.")
-        spec.input("forceFieldFile", valid_type=SinglefileData, required=False, help="A file containing the force field parameters for the ChemShell MM interface.")
+        spec.input("optimisation_parameters", valid_type=Dict, validator=cls.validate_optimisation_parameters, required=False, help="A dictionary of parameters for the ChemShell geometry optimisation task. If this input is provided, a geometry optimisation task will be configured and added to this job.")
 
-        spec.output("energy", valid_type=Float, help="The total energy of the system.")
+        spec.input("QM_parameters", valid_type=Dict, validator=cls.validate_QM_parameters, required=False, help="A dictionary of parameters for to be passed to the Theory object for the ChemShell calculation.")
+        
+        spec.input("MM_parameters", valid_type=Dict, validator=cls.validate_MM_parameters, required=False, help="A dictionary of parameters for the ChemShell MM interface.")
+        spec.input("forceFieldFile", valid_type=SinglefileData, required=False, help="A file containing the force field parameters for the ChemShell MM interface.")
+        
+        spec.input("QMMM_parameters", valid_type=Dict, required=False, help="A dictionary of parameters for the ChemShell QM/MM interface.")
+        
+
+        spec.output("energy", valid_type=Float, required=True, help="The total energy of the system.")
+        spec.output("optimised_structure", valid_type=SinglefileData, required=False, help="The optimised structure of the given system, if a geometry optimisation task was configured and successfully completed. The structure is contained within a ChemShell '.pun' file.")
 
         spec.inputs["metadata"]["options"]["resources"].default = {"num_machines": 1, "num_mpiprocs_per_machine": 1}
         spec.inputs["metadata"]["options"]["parser_name"].default = "chemshell"
+
+
 
     @classmethod 
     def get_valid_calculation_parameter_keys(cls) -> tuple[str]:
@@ -73,6 +83,45 @@ class ChemShellCalculation(CalcJob):
         if "hessian" in value.keys():
             if not isinstance(value.get("hessian"), bool):
                 return "The 'hessian' parameter must be a boolean value (True or False)."
+        
+        return
+    
+    @classmethod 
+    def get_valid_optimisation_parameter_keys(cls) -> tuple[str]:
+        """
+        Return a tuple of valid parameter keys for the ChemShell geometry optimisation task.
+
+        Returns
+        -------
+        validKeys : tuple[str]
+            A tuple of valid optimisation parameter keys for the ChemShell calculation.
+        """
+        validKeys = ("maxcycle", "maxene", "coordinates", "algorithm", "trust_radius", "maxstep", "tolerance", "neb", "nimages", "nebk", "dimer", "delta", "tsrelative")
+        # other options not included thus far: redidues, contraints, frag2
+        return validKeys
+    
+    @classmethod
+    def validate_optimisation_parameters(cls, value: Dict | None, _) -> str | None:
+        """
+        Validate the optimisation parameters to be passed to the ChemShell geometry optimisation task.
+        
+        Parameters
+        ----------
+        value : Dict | None
+            A dictionary of parameters for the ChemShell geometry optimisation task. If None, no validation is performed.
+
+        Returns 
+        -------
+        str | None
+            Returns None if the parameters are valid, otherwise returns an error message string.
+        """
+        
+        invalidKeys = set(value.keys()).difference(set(cls.get_valid_optimisation_parameter_keys()))
+        if invalidKeys:
+            return "The following parameter keys are invalid: {0:s}. Valid keys are: {1:s}".format(
+                ", ".join(invalidKeys), 
+                ", ".join(cls.get_valid_optimisation_parameter_keys())
+            )
         
         return
 
@@ -284,27 +333,51 @@ class ChemShellCalculation(CalcJob):
                     # '' 
                 )
 
-        if "calculation_parameters" not in self.inputs:
-            self.inputs.calculation_parameters = Dict(dict={})
-        
-        if qmTheory and not mmTheory:
-            script += "SP(theory=qmtheory, gradients={0:s}, hessian={1:s}).run()\n".format(
-                str(self.inputs.calculation_parameters.get("gradients", False)),
-                str(self.inputs.calculation_parameters.get("hessian", False))
+        if "optimisation_parameters" in self.inputs:
+            # Run a geometry optimisation using DL_FIND 
+            optStr = "Opt(theory=qmtheory, maxcycle={0:d}, maxene={1:d}, coordinates='{2:s}', algorithm='{3:s}', "
+            optStr += "trust_radius='{4:s}', maxstep={5:f}, tolerance={6:f}, neb='{7:s}', nimages={8:d}, nebk={9:f}, "
+            optStr += "dimer={10:s}, delta={11:f}, tsrelative={12:s}).run()\n"
+            optStr = optStr.format(
+                self.inputs.optimisation_parameters.get("maxcycle", 100),
+                self.inputs.optimisation_parameters.get("maxene", 10000),
+                self.inputs.optimisation_parameters.get("coordinates", "cartesian"),
+                self.inputs.optimisation_parameters.get("algorithm", "lbfgs"),
+                self.inputs.optimisation_parameters.get("trust_radius", "constant"), 
+                self.inputs.optimisation_parameters.get("maxstep", 0.5), 
+                self.inputs.optimisation_parameters.get("tolerance", 0.00045), 
+                self.inputs.optimisation_parameters.get("neb", ""), 
+                self.inputs.optimisation_parameters.get("nimages", 1), 
+                self.inputs.optimisation_parameters.get("nebk", 0.01), 
+                str(self.inputs.optimisation_parameters.get("dimer", False)),
+                self.inputs.optimisation_parameters.get("delta", 0.01), 
+                str(self.inputs.optimisation_parameters.get("tsrelative", False))
             )
-        elif mmTheory and not qmTheory:
-            script += "SP(theory=mmtheory, gradients={0:s}, hessian={1:s}).run()\n".format(
-                str(self.inputs.calculation_parameters.get("gradients", False)),
-                str(self.inputs.calculation_parameters.get("hessian", False))
-            )
-        elif qmTheory and mmTheory:
-            if self.input.QMMM_parameters:
-                script += "from chemsh import QMMM\n"
-                script += "qmmm = QMMM(frag=structure, qm=qmtheory, mm=mmtheory, qm_region=[{0:s}])\n".format('')
-                script += "SP(theory=qmmm, gradients={0:s}, hessian={1:s}).run()\n".format(
+            script += optStr
+        else:
+            # Perform a single point energy calculation 
+            if "calculation_parameters" not in self.inputs:
+                # Assign default values if none are given 
+                self.inputs.calculation_parameters = Dict(dict={})
+            
+            if qmTheory and not mmTheory:
+                script += "SP(theory=qmtheory, gradients={0:s}, hessian={1:s}).run()\n".format(
                     str(self.inputs.calculation_parameters.get("gradients", False)),
                     str(self.inputs.calculation_parameters.get("hessian", False))
                 )
+            elif mmTheory and not qmTheory:
+                script += "SP(theory=mmtheory, gradients={0:s}, hessian={1:s}).run()\n".format(
+                    str(self.inputs.calculation_parameters.get("gradients", False)),
+                    str(self.inputs.calculation_parameters.get("hessian", False))
+                )
+            elif qmTheory and mmTheory:
+                if self.input.QMMM_parameters:
+                    script += "from chemsh import QMMM\n"
+                    script += "qmmm = QMMM(frag=structure, qm=qmtheory, mm=mmtheory, qm_region=[{0:s}])\n".format('')
+                    script += "SP(theory=qmmm, gradients={0:s}, hessian={1:s}).run()\n".format(
+                        str(self.inputs.calculation_parameters.get("gradients", False)),
+                        str(self.inputs.calculation_parameters.get("hessian", False))
+                    )
         return script 
 
 
@@ -339,8 +412,13 @@ class ChemShellCalculation(CalcJob):
         calcInfo.retrieve_list = [ChemShellCalculation.FILE_STDOUT,]
         calcInfo.local_copy_list = [
             (self.inputs.structure.uuid, self.inputs.structure.filename, self.inputs.structure.filename),
-            (self.inputs.forceFieldFile.uuid, self.inputs.forceFieldFile.filename, self.inputs.forceFieldFile.filename),
         ]
+
+        if "MM_parameters" in self.inputs:
+            calcInfo.local_copy_list.append((self.inputs.forceFieldFile.uuid, self.inputs.forceFieldFile.filename, self.inputs.forceFieldFile.filename))
+
+        if "optimisation_parameters" in self.inputs:
+            calcInfo.retrieve_list.append(ChemShellCalculation.FILE_DLFIND)
 
 
         return calcInfo 
--- src/aiida_chemshell/parsers.py
+++ src/aiida_chemshell/parsers.py
@@ -1,5 +1,8 @@
+from aiida.engine import ExitCode
+from aiida.orm import Float, SinglefileData
 from aiida.parsers.parser import Parser 
-from aiida.orm import Float
+
+from aiida_chemshell.calculations import ChemShellCalculation 
 
 class ChemShellParser(Parser):
     """
@@ -11,12 +14,21 @@ class ChemShellParser(Parser):
         Parse the output of a ChemShell calculation.
         """
 
-        # Read the single point energy from the ChemShell output 
-        with self.retrieved.open("output.log", 'r') as f:
-            for line in f:
-                if "Final SP energy" in line:
-                    energy = Float(line.split()[4])
-                    self.out("energy", energy)
-                    break 
-
+        if "optimisation_parameters" in self.node.inputs:
+            # Store the optimised structure file 
+            with self.retrieved.open(ChemShellCalculation.FILE_DLFIND, 'r') as f:
+                self.out("optimised_structure", SinglefileData(file=f))
+            # Read the final converged energy value for the optimised structure 
+            with self.retrieved.open(ChemShellCalculation.FILE_STDOUT, 'r') as f:
+                for line in f:
+                    if "Final converged energy" in line:
+                        self.out("energy", Float(line.split()[3]))
+                        break 
+        else:
+            # Read the single point energy from the ChemShell output 
+            with self.retrieved.open(ChemShellCalculation.FILE_STDOUT, 'r') as f:
+                for line in f:
+                    if "Final SP energy" in line:
+                        self.out("energy", Float(line.split()[4]))
+                        break
         

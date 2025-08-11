90d33f9662b0107e8ae859abb92e707a9bfefb29
Benjamin Speake <benjaminspeake@gmail.com>
2025-08-11 10:06:43 +0100

    Configure CI pipelines for build and test stages (missing files)

---
+++ utils/version_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+GIT_TAG=$(git describe --tags --abbrev=0)
+echo "Git tag: $GIT_TAG" 
+APP_VERSION=$(python -c "
+from aiida_chemshell import __version__
+print(__version__) 
+")
+echo "Package version: $APP_VERSION" 
+
+if [ "${GIT_TAG:1}" != "$APP_VERSION" ]; then 
+  echo "ERROR: git tag does not match package version string" 
+  exit 1 
+fi 

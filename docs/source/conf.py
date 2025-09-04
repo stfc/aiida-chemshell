d47f2ea83823ce9a6d3c88c6c209156c5b6a55fe
Benjamin Speake <benjaminspeake@gmail.com>
2025-09-04 14:00:41 +0100

    Fix theme error in docs build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,5 +33,5 @@ html_logo = ""
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme' # Same as ChemShell documentation 
+html_theme = 'piccolo_theme'
 html_static_path = ['_static']

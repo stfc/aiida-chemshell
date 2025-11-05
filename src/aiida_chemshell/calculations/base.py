233d50bacc56539218e9acb488bbdbc4b40a5307
Benjamin Speake <benjaminspeake@gmail.com>
2025-11-05 16:25:11 +0000

    Correct names for trajectory path files

--- src/aiida_chemshell/calculations/base.py
+++ src/aiida_chemshell/calculations/base.py
@@ -23,8 +23,8 @@ class ChemShellCalculation(CalcJob):
     FILE_DLFIND = "_dl_find.cjson"
     FILE_TMP_STRUCTURE = "input_structure.xyz"
     FILE_RESULTS = "result.json"
-    FILE_TRJPTH = "_dl_find/path.xyz"
-    FILE_TRJFRC = "_dl_find/force.xyz"
+    FILE_TRJPTH = "path.xyz"
+    FILE_TRJFRC = "path_force.xyz"
 
     @classmethod
     def define(cls, spec: CalcJobProcessSpec) -> None:

c33f0c58ea6a164080223594d34e330577eade2b
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2026-06-18 09:04:06 +0100

    Simplify isolated atoms workflow and extract re-usable parts

--- src/aiida_chemshell/units.py
+++ src/aiida_chemshell/units.py
@@ -5,6 +5,7 @@ class UnitsConverter:
     """Unit conversion utility."""
 
     ANGSTROM = 0.529177
+    EV = 27.2114
 
     @classmethod
     def angstrom_to_bohr(cls, val: float) -> float:
@@ -15,3 +16,13 @@ class UnitsConverter:
     def bohr_to_angstrom(cls, val: float) -> float:
         """Convert bohr (a.u.) to Angstrom."""
         return val * UnitsConverter.ANGSTROM
+
+    @classmethod
+    def hartree_to_ev(cls, val: float) -> float:
+        """Convert Hartree (a.u.) to eV."""
+        return val * UnitsConverter.EV
+
+    @classmethod
+    def ev_to_hartree(cls, val: float) -> float:
+        """Convert eV to Hartree (a.u.)."""
+        return val / UnitsConverter.EV

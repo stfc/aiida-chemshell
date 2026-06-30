97b99b3c7d9bfab779308f332e113670933d268e
Benjamin T. Speake <benjamin.speake@stfc.ac.uk>
2026-06-30 17:03:15 +0100

    Create test for BatchProcessWorkChain where both "trajectory" and "structures" inputs are specified

--- tests/test_batch_workcahin.py
+++ tests/test_batch_workcahin.py
@@ -21,11 +21,15 @@ def test_batch_from_trajectorydata(chemsh_code, water_trajectory_object):
     }
     results, node = run_get_node(BatchProcessWorkChain, **inputs)
 
+    assert node.is_finished_ok, "WorkChain Failed"
+
     sub_nodes = node.called
+    assert len(sub_nodes) == 3, "Incorrect number of sub processes created."
 
     final_energies = [-75.565560193461, -75.585287771819, -75.426355430539]
 
     for i, sub_node in enumerate(sub_nodes):
+        assert sub_node.is_finished_ok, "Sub Process Failed"
         assert abs(sub_node.outputs.energy - final_energies[i]) < 1e-10
 
 
@@ -50,9 +54,49 @@ def test_batch_from_structuredata(chemsh_code, water_trajectory_object):
     }
     results, node = run_get_node(BatchProcessWorkChain, **inputs)
 
+    assert node.is_finished_ok, "WorkChain Failed"
+
     sub_nodes = node.called
+    assert len(sub_nodes) == 3, "Incorrect number of sub processes created."
 
     final_energies = [-75.565560193461, -75.585287771819, -75.426355430539]
 
     for i, sub_node in enumerate(sub_nodes):
+        assert sub_node.is_finished_ok, "Sub Process Failed"
+        assert abs(sub_node.outputs.energy - final_energies[i]) < 1e-10
+
+
+def test_batch_from_structuredata_and_trajectorydata(
+    chemsh_code, water_trajectory_object, water_structure_object
+):
+    """DFT based single point test."""
+    inputs = {
+        "code": chemsh_code(),
+        "trajectory": water_trajectory_object,
+        "structures": {
+            "Structure_1": water_structure_object,
+        },
+        "qm_parameters": Dict(
+            {
+                "theory": "PySCF",
+                "method": "hf",
+            }
+        ),
+    }
+    results, node = run_get_node(BatchProcessWorkChain, **inputs)
+
+    assert node.is_finished_ok, "WorkChain Failed"
+
+    sub_nodes = node.called
+    assert len(sub_nodes) == 4, "Incorrect number of sub processes created."
+
+    final_energies = [
+        -75.565560193461,
+        -75.585287771819,
+        -75.426355430539,
+        -75.585287771819,
+    ]
+
+    for i, sub_node in enumerate(sub_nodes):
+        assert sub_node.is_finished_ok, "Sub Process Failed"
         assert abs(sub_node.outputs.energy - final_energies[i]) < 1e-10
